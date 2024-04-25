d6edce1b4b80dac505604d850b7c0cf2e69f8016
Delaey Emiel <emiel.delaey@student.howest.be>
2024-04-25 13:27:15 +0200

    added heading

--- src/components/TipTap.vue
+++ src/components/TipTap.vue
@@ -22,14 +22,45 @@
                 :class="{ 'is-active': editor?.isActive('bulletList') }">
           <svg-icon type="mdi" :path="icons.bulletList"></svg-icon>
         </button>
-      </div>
-      <div class="join">  <!-- Row 2 -->
-        <button @click="editor?.chain().focus().undo().run()" :disabled="!editor?.can().chain().focus().undo().run()">
+        <button @click="editor?.chain().focus().toggleOrderedList().run()" class="btn btn-sm join-item"
+                :class="{ 'is-active': editor?.isActive('orderedList') }">
+          <svg-icon type="mdi" :path="icons.orderedList"></svg-icon>
+        </button>
+        <button @click="editor?.chain().focus().setHorizontalRule().run()" class="btn btn-sm join-item">
+          <svg-icon type="mdi" :path="icons.horizontalRule"></svg-icon>
+        </button>
+        <button @click="editor?.chain().focus().undo().run()" :disabled="!editor?.can().chain().focus().undo().run()"
+        class="ml-2 pl-2 b-l border-l">
           <svg-icon type="mdi" :path="icons.undo"></svg-icon>
         </button>
         <button @click="editor?.chain().focus().redo().run()" :disabled="!editor?.can().chain().focus().redo().run()">
           <svg-icon type="mdi" :path="icons.redo" ></svg-icon>
         </button>
+      </div>
+      <div class="join">  <!-- Row 2 -->
+        <button @click="editor?.chain().focus().setTextAlign('left').run()" class="btn btn-sm join-item"
+                :class="{ 'is-active': editor?.isActive({ textAlign: 'left' }) }">
+          <svg-icon type="mdi" :path="icons.alignLeft" ></svg-icon>
+        </button>
+        <button @click="editor?.chain().focus().setTextAlign('center').run()" class="btn btn-sm join-item"
+                :class="{ 'is-active': editor?.isActive({ textAlign: 'center' }) }">
+          <svg-icon type="mdi" :path="icons.alignCenter" ></svg-icon>
+        </button>
+        <button @click="editor?.chain().focus().setTextAlign('right').run()" class="btn btn-sm join-item"
+                :class="{ 'is-active': editor?.isActive({ textAlign: 'right' }) }">
+          <svg-icon type="mdi" :path="icons.alignRight" ></svg-icon>
+        </button>
+        <template v-for="level in [2, 3, 4]" :key="level">
+          <button @click="editor?.chain().focus().toggleHeading({ level: level }).run()" :class="{ 'is-active': editor?.isActive('heading', { level: level }) }">
+            <svg-icon
+                :key="level"
+                type="mdi"
+                size="20"
+                :path="icons[`h${level}`]"
+            ></svg-icon>
+          </button>
+        </template>
+
       </div>
 
 
@@ -44,32 +75,44 @@
 button:disabled svg{
   @apply text-gray-200;
 }
+details ul{
+  @apply m-0;
+}
+h2{
+  @apply text-2xl;
+}
 
 </style>
 <script lang="ts">
-import StarterKit from '@tiptap/starter-kit'
-import { Editor, EditorContent } from '@tiptap/vue-3'
+import {StarterKit} from "@tiptap/starter-kit";
+import {Editor, EditorContent, mergeAttributes} from '@tiptap/vue-3'
 import SvgIcon from '@jamescoyle/vue-icon';
 import Underline from '@tiptap/extension-underline'
 import BulletList from '@tiptap/extension-bullet-list'
 import ListItem from '@tiptap/extension-list-item'
-import { mdiFormatBold, mdiFormatItalic, mdiFormatUnderline, mdiUndo, mdiRedo, mdiFormatStrikethroughVariant, mdiFormatListBulleted } from '@mdi/js';
+import OrderedList from '@tiptap/extension-ordered-list'
+import HorizontalRule from '@tiptap/extension-horizontal-rule'
+import TextAlign from '@tiptap/extension-text-align'
+import DropCursor from '@tiptap/extension-dropcursor'
+import Heading from '@tiptap/extension-heading';
+import Typography from "@tiptap/extension-typography";
+import { mdiFormatBold, mdiFormatItalic, mdiFormatUnderline, mdiUndo, mdiRedo, mdiFormatStrikethroughVariant,
+  mdiFormatListBulleted, mdiFormatListNumbered, mdiMinus, mdiAlignHorizontalLeft, mdiAlignHorizontalCenter,
+  mdiAlignHorizontalRight, mdiFormatHeader1, mdiFormatHeader2, mdiFormatHeader3, mdiFormatHeader4}
+  from '@mdi/js';
 export default {
   name: 'TipTap',
   components: {
     EditorContent,
     SvgIcon
   },
-
   props: {
     modelValue: {
       type: String,
       default: '',
     },
   },
-
   emits: ['update:modelValue'],
-
   data() : { editor: any | Editor; icons: any } {
     return {
       editor: null,
@@ -80,7 +123,14 @@ export default {
         undo: mdiUndo,
         redo: mdiRedo,
         strike: mdiFormatStrikethroughVariant,
-        bulletList: mdiFormatListBulleted
+        bulletList: mdiFormatListBulleted,
+        orderedList: mdiFormatListNumbered,
+        horizontalRule: mdiMinus,
+        alignLeft: mdiAlignHorizontalLeft,
+        alignCenter: mdiAlignHorizontalCenter,
+        alignRight: mdiAlignHorizontalRight,
+        h1: mdiFormatHeader1, h2: mdiFormatHeader2, h3: mdiFormatHeader3, h4: mdiFormatHeader4,
+
       },
     }
   },
@@ -97,7 +147,6 @@ export default {
       this.editor?.commands.setContent(value, false)
     },
   },
-
   mounted() {
     this.editor = new Editor({
       editorProps: {
@@ -113,14 +162,52 @@ export default {
           Underline,
           BulletList.configure({
             HTMLAttributes: {
-              class: 'list-disc ml-4 leading-normal flex flex-col'
+              class: 'list-disc ml-4 leading-normal'
             }
           }),
           ListItem.configure({
             HTMLAttributes: {
               class: 'list-item leading-4'
             }
-          })
+          }),
+          OrderedList.configure({
+            HTMLAttributes: {
+              class: 'list-decimal ml-4 leading-normal'
+            }
+          }),
+          HorizontalRule.configure({
+            HTMLAttributes: {
+              class: 'my-4'
+            }
+          }),
+          TextAlign.configure({
+            types: ['heading', 'paragraph'],
+            alignments: ['left', 'center', 'right']
+          }),
+          DropCursor,
+          Heading.configure({
+            levels: [2, 3, 4],
+          }).extend({
+            levels: [2, 3, 4],
+            renderHTML({ node, HTMLAttributes }) {
+              const level: number = this.options.levels.includes(node.attrs.level)
+                  ? node.attrs.level
+                  : this.options.levels[0]
+              const classes: any = {
+                2: 'text-xl',
+                3: 'text-md font-bold',
+                4: 'text-md',
+              }
+              return [
+                `h${level}`,
+                mergeAttributes(this.options.HTMLAttributes, HTMLAttributes, {
+                  class: `${classes[level]}`,
+                }),
+                0,
+              ]
+            },
+          }),
+          Typography
       ],
       content: this.modelValue,
       onUpdate: () => {
@@ -130,7 +217,7 @@ export default {
   },
 
   beforeUnmount() {
-    this.editor.destroy()
+    this.editor?.destroy()
   },
 }
 </script>
